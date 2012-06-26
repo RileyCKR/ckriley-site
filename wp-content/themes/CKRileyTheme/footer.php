625a063ff8981d716a2c57a30ca4a64741fdb4aa
Chris Riley <RileyCKR@gmail.com>
2012-06-25 23:09:27 -0400

    Moved google analytics code to footer

--- wp-content/themes/CKRileyTheme/footer.php
+++ wp-content/themes/CKRileyTheme/footer.php
@@ -9,5 +9,18 @@
 	</footer></div><!--#footer-->
 </div><!--#main-->
 <?php wp_footer(); /* this is used by many Wordpress features and plugins to work proporly */ ?>
+<?php if ( ! is_user_logged_in() ) : ?>
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-32920793-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+<?php endif; ?>
 </body>
 </html>
--- wp-content/themes/CKRileyTheme/header.php
+++ wp-content/themes/CKRileyTheme/header.php
@@ -36,20 +36,6 @@
 	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'template_url' ); ?>/lessframework.css" />
 	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'template_url' ); ?>/theme.css" />
 	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-	
-	<?php if ( ! is_user_logged_in() ) { ?>
-	<script type="text/javascript">
-	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-32920793-1']);
-	  _gaq.push(['_trackPageview']);
-
-	  (function() {
-		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
-	</script>
-	<?php } ?>
 </head>
 
 <body <?php body_class(); ?>>
