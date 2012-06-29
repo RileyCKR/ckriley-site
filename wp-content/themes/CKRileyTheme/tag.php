6a4994a5459a736c6f600104569ef42f98ec53f6
Chris Riley <RileyCKR@gmail.com>
2012-06-28 22:19:52 -0400

    Removed author

--- wp-content/themes/CKRileyTheme/tag.php
+++ wp-content/themes/CKRileyTheme/tag.php
@@ -13,7 +13,7 @@
 			</div>
 	
 			<div class="post-meta">
-				<p><?php _e('Written on '); the_time('F j, Y'); _e(' at '); the_time(); _e(', by '); the_author_posts_link() ?></p>
+				<p><?php _e('Written on '); the_time('F j, Y'); _e(' at '); the_time(); ?></p>
 				<p><?php if (the_tags('Tags: ', ', ', ' ')); ?></p>
 			</div><!--.postMeta-->
 		</div><!--.1`post-single-->
